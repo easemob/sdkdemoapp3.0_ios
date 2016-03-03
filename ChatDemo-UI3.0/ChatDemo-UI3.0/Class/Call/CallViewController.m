cd9e58a0958a01096031b0d3b27a2469c2a2ff5d
李子龙 <15131968@qq.com>
2016-03-03 15:37:41 +0800

    fix:do not send hasRead when end call

--- ChatDemo-UI3.0/ChatDemo-UI3.0/Class/Call/CallViewController.m
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Class/Call/CallViewController.m
@@ -380,9 +380,8 @@
 
 - (void)close
 {
-    _callSession.remoteView.hidden = YES;
-    _callSession.localView.hidden = YES;
     _callSession = nil;
+    _callSession.remoteView.hidden = YES;
     _propertyView = nil;
     
     if (_timeTimer) {
@@ -394,6 +393,8 @@
         [_propertyTimer invalidate];
         _propertyTimer = nil;
     }
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:KNOTIFICATION_CALL object:nil];
     
     dispatch_async(dispatch_get_main_queue(), ^{
         [[AVAudioSession sharedInstance] setActive:NO error:nil];
