6a1fd243f68341b24d59db6730f88b3ea45afbea
sxqxyzw <sxqxwang@126.com>
2016-03-07 18:09:33 +0800

    add ReadFireCell

---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Class/Chat/ChatView/Cell/ReadFireCell.h
@@ -0,0 +1,16 @@
+//
+//  ReadFireCell.h
+//  ChatDemo-UI3.0
+//
+//  Created by EaseMob on 16/1/18.
+//  Copyright © 2016年 EaseMob. All rights reserved.
+//
+
+#import "EaseBaseMessageCell.h"
+#import "EaseCustomMessageCell.h"
+
+@interface ReadFireCell : EaseBaseMessageCell
+
+- (void)isReadMessage:(BOOL)isRead;
+
+@end
