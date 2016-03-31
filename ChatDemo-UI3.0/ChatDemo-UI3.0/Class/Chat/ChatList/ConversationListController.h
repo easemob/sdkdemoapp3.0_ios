b56c626fa540f6371996e76dc7624082c566e0e7
lizilong1989 <15131968@qq.com>
2016-03-31 22:33:33 +0800

    update technologies to inc

--- ChatDemo-UI3.0/ChatDemo-UI3.0/Class/Chat/ChatList/ConversationListController.h
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Class/Chat/ChatList/ConversationListController.h
@@ -1,13 +1,13 @@
 /************************************************************
  *  * Hyphenate CONFIDENTIAL
  * __________________
- * Copyright (C) 2016 Hyphenate Technologies. All rights reserved.
+ * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
  *
  * NOTICE: All information contained herein is, and remains
- * the property of Hyphenate Technologies.
+ * the property of Hyphenate Inc.
  * Dissemination of this information or reproduction of this material
  * is strictly forbidden unless prior written permission is obtained
- * from Hyphenate Technologies.
+ * from Hyphenate Inc.
  */
 
 @interface ConversationListController : EaseConversationListViewController
