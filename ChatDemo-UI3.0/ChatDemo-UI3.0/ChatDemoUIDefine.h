ad02cb3bb47105f7f12aedbd1ea13fef7fbafca4
Jerry Wu <jerrywu12@gmail.com>
2016-06-03 15:01:11 -0700

    remove unused color setter

--- ChatDemo-UI3.0/ChatDemo-UI3.0/ChatDemoUIDefine.h
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/ChatDemoUIDefine.h
@@ -15,9 +15,6 @@
 
 #define IS_IPHONE_5 ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
 
-#define RGBACOLOR(r,g,b,a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)]
-
 #define KNOTIFICATION_LOGINCHANGE @"loginStateChange"
 
-#define CHATVIEWBACKGROUNDCOLOR [UIColor colorWithRed:0.936 green:0.932 blue:0.907 alpha:1]
 #endif
