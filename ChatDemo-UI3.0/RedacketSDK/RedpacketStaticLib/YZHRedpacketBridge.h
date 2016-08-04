741d43baf31bf7fff5f4d94845aed4992db46506
youran1024 <youran1024@163.com>
2016-08-04 10:52:23 +0800

    【红包】红包3.1.0版本，增加通过后端配置项修改前端部分功能

--- ChatDemo-UI3.0/RedacketSDK/RedpacketStaticLib/YZHRedpacketBridge.h
+++ ChatDemo-UI3.0/RedacketSDK/RedpacketStaticLib/YZHRedpacketBridge.h
@@ -38,18 +38,23 @@
 /* 以下2种方法，根据IM选择其一 */
 
 /**
- *  Method1:通过签名的方式获取Token (以下参数均需要由AppServer提供签名接口支持，签名方法见集成文档)
+ *  Method1:通过签名的方式获取Token (以下参数的获取方式见RestAPI集成文档)
  *  此方法目前适应于：腾讯IM， 未适配的可联系我们
  *
- *  @param sign       签名接口返回参数
- *  @param partner    签名接口返回参数
- *  @param appUserid  签名接口返回参数 - 用户在App的用户ID
- *  @param timeStamp  签名接口返回参数 - 时间戳
+ *  @param sign
+ *  @param partner
+ *  @param appUserid  用户在App的用户ID
+ *  @param timeStamp  时间戳
  */
 - (void)configWithSign:(NSString *)sign
                partner:(NSString *)partner
              appUserId:(NSString *)appUserid
-             timeStamp:(long)timeStamp;
+             timestamp:(NSString *)timestamp;
+
+- (void)configWithSign:(NSString *)sign
+               partner:(NSString *)partner
+             appUserId:(NSString *)appUserid
+             timeStamp:(long)timeStamp __attribute__((deprecated("方法命名不规范，已经停用, 请使用上边的方法")));
 
 /**
  *  Method2: 通过环信imToken的方式获取Token
@@ -66,6 +71,7 @@
  *  用户退出需要清空Token
  */
 - (void)redpacketUserLoginOut __attribute__((deprecated("方法已经不需要调用, SDK根据用户变更和Token过期自动切换")));
+
 
 /**
  *  请求Token
