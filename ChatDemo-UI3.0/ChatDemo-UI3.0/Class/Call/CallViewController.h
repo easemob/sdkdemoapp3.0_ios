ede9c99bbcf4942910bc8e145b34e42cec1f33ee
XieYajie <xyjxyf@163.com>
2016-09-19 17:06:52 +0800

    添加摄像头切换操作

--- ChatDemo-UI3.0/ChatDemo-UI3.0/Class/Call/CallViewController.h
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Class/Call/CallViewController.h
@@ -38,7 +38,7 @@
     UIButton *_videoButton;
     UIButton *_voiceButton;
 //    UIButton *_recordButton;
-//    UIButton *_switchCameraButton;
+    UIButton *_switchCameraButton;
 }
 
 @property (strong, nonatomic) UILabel *statusLabel;
