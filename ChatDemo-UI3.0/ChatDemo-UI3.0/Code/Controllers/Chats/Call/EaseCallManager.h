3e733a4d4a6123548e96d470d7f64d122238b5be
drunkbread <853163939@qq.com>
2016-10-08 16:11:27 +0800

    add voice call

---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Code/Controllers/Chats/Call/EaseCallManager.h
@@ -0,0 +1,13 @@
+//
+//  EaseCallManager.h
+//  ChatDemo-UI3.0
+//
+//  Created by EaseMob on 16/9/29.
+//  Copyright © 2016年 easemob. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EaseCallManager : NSObject
+
+@end
---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Code/Controllers/Chats/Call/EaseCallManager.m
@@ -0,0 +1,13 @@
+//
+//  EaseCallManager.m
+//  ChatDemo-UI3.0
+//
+//  Created by EaseMob on 16/9/29.
+//  Copyright © 2016年 easemob. All rights reserved.
+//
+
+#import "EaseCallManager.h"
+
+@implementation EaseCallManager
+
+@end
