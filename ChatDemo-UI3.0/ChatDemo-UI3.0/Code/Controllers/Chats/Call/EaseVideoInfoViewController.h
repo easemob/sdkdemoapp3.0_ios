98fa1b7d08b8fc5aa97221cce124c569dbd723aa
drunkbread <853163939@qq.com>
2016-10-09 16:06:27 +0800

    add video call

---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Code/Controllers/Chats/Call/EaseVideoInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  EaseVideoInfoViewController.h
+//  ChatDemo-UI3.0
+//
+//  Created by EaseMob on 16/10/9.
+//  Copyright © 2016年 easemob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EaseVideoInfoViewController : UIViewController
+
+@end
---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Code/Controllers/Chats/Call/EaseVideoInfoViewController.m
@@ -0,0 +1,13 @@
+//
+//  EaseVideoInfoViewController.m
+//  ChatDemo-UI3.0
+//
+//  Created by EaseMob on 16/10/9.
+//  Copyright © 2016年 easemob. All rights reserved.
+//
+
+#import "EaseVideoInfoViewController.h"
+
+@implementation EaseVideoInfoViewController
+
+@end
