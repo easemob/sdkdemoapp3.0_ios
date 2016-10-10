3ba904d8e06293a5ca40b4f26df4e5e78777f74e
drunkbread <853163939@qq.com>
2016-10-10 16:40:11 +0800

    complete call

--- ChatDemo-UI3.0/ChatDemo-UI3.0/Code/Controllers/Chats/Call/EaseVideoInfoViewController.h
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Code/Controllers/Chats/Call/EaseVideoInfoViewController.h
@@ -2,7 +2,7 @@
 //  EaseVideoInfoViewController.h
 //  ChatDemo-UI3.0
 //
-//  Created by EaseMob on 16/10/9.
+//  Created by EaseMob on 16/10/10.
 //  Copyright © 2016年 easemob. All rights reserved.
 //
 
--- ChatDemo-UI3.0/ChatDemo-UI3.0/Code/Controllers/Chats/Call/EaseVideoInfoViewController.m
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Code/Controllers/Chats/Call/EaseVideoInfoViewController.m
@@ -2,12 +2,36 @@
 //  EaseVideoInfoViewController.m
 //  ChatDemo-UI3.0
 //
-//  Created by EaseMob on 16/10/9.
+//  Created by EaseMob on 16/10/10.
 //  Copyright © 2016年 easemob. All rights reserved.
 //
 
 #import "EaseVideoInfoViewController.h"
 
+@interface EaseVideoInfoViewController ()
+
+@end
+
 @implementation EaseVideoInfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
