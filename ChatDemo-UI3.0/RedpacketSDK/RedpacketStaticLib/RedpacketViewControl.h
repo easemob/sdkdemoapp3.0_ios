0217fec67fc5ffc281354f55f71e51c8cd83851b
Yanhu <yanyouchi@163.com>
2016-12-02 20:15:04 +0800

    【红包】更新红包SDK

--- ChatDemo-UI3.0/RedpacketSDK/RedpacketStaticLib/RedpacketViewControl.h
+++ ChatDemo-UI3.0/RedpacketSDK/RedpacketStaticLib/RedpacketViewControl.h
@@ -10,10 +10,39 @@
 #import <UIKit/UIKit.h>
 #import "RedpacketMessageModel.h"
 
+typedef NS_ENUM(NSInteger,RPSendRedPacketViewControllerType){
+    RPSendRedPacketViewControllerSingle, //点对点红包
+    RPSendRedPacketViewControllerGroup,  //普通群红包
+    RPSendRedPacketViewControllerMember, //专属红包（包含普通群红包功能）
+    RPSendRedPacketViewControllerRand,   //小额度随机红包
+};
+
+@protocol RedpacketViewControlDelegate <NSObject>
+
+@optional
+/**
+ *  获取定向(专属)红包中的群成员列表
+ */
+- (void)getGroupMemberListCompletionHandle:(void (^)(NSArray<RedpacketUserInfo *> * groupMemberList))completionHandle;
+
+/**
+ *  广告红包事件触发，需深度合作。具体请联系商务
+ *  客服热线: 400-6565-739
+ *  业务咨询: BD@yunzhanghu.com
+ */
+- (void)advertisementRedPacketAction:(NSDictionary *)args;
+
+- (NSArray<RedpacketUserInfo *> *)groupMemberList __deprecated_msg("请用getGroupMemberListCompletionHandle：方法替换");
+
+@end
+
+
 //  抢红包成功回调
 typedef void(^RedpacketGrabBlock)(RedpacketMessageModel *messageModel);
-//  环信接口发送红包消息回调
+
+//  接口发送红包消息回调
 typedef void(^RedpacketSendBlock)(RedpacketMessageModel *model);
+
 
 /**
  *  发红包的控制器
@@ -31,7 +60,17 @@ typedef void(^RedpacketSendBlock)(RedpacketMessageModel *model);
 @property (nonatomic, weak) UIViewController *conversationController;
 
 /**
- *  用户单击了聊天窗口中的红包Cell后触发
+ *  定向红包获取群成员的代理
+ */
+@property (nonatomic, weak) id <RedpacketViewControlDelegate> delegate;
+
+/**
+ *  零钱接口返回零钱
+ */
++ (void)getChangeMoney:(void (^)(NSString *amount))amount;
+
+/**
+ *  用户抢红包触发事件
  *
  *  @param messageModel 消息Model
  */
@@ -45,54 +84,66 @@ typedef void(^RedpacketSendBlock)(RedpacketMessageModel *model);
  */
 - (void)setRedpacketGrabBlock:(RedpacketGrabBlock)grabTouch andRedpacketBlock:(RedpacketSendBlock)sendBlock;
 
-#pragma mark - Controllers
+@end
 
-/**
- *  点对点红包控制器
- *
- *  @return 返回红包控制器
- */
-- (UIViewController *)redpacketViewController;
 
-/**
- *  多人红包控制器
- *
- *  @param changeMoneyController 聊天室或者群人数
- *
- *  @return 返回多人红包控制器
- */
-- (UIViewController *)mulityRedpacketViewController:(int)peopleCount;
+@interface RedpacketViewControl (RedpacketControllers)
 
 /**
  *  零钱页面
- *
- *  @return 零钱页面，App可以放在需要的位置
  */
 + (UIViewController *)changeMoneyController;
 
 /**
  *  零钱明细页面
- *
- *  @return 零钱明细页面，App可以放在需要的位置
  */
 + (UIViewController *)changeMoneyListController;
 
-#pragma mark - ShowViewControllers
-
 /**
- *  显示群红包页面
+ *  生成红包页面
+ *
+ *  @param rpType 红包页面类型
+ *  @param count  群红包群人数， 如果是点对点红包传入0
  */
-- (void)presentRedPacketMoreViewControllerWithCount:(int)count;
+- (UIViewController *)redPacketViewControllerWithType:(RPSendRedPacketViewControllerType)rpType memberCount:(NSInteger)count;
 
 /**
- *  显示点对点红包页面
+ *  弹出发红包页面
+ *
+ *  @param rpType 红包页面类型
+ *  @param count  群红包群人数， 如果是点对点红包传入0
  */
-- (void)presentRedPacketViewController;
+- (void)presentRedPacketViewControllerWithType:(RPSendRedPacketViewControllerType)rpType memberCount:(NSInteger)count;
 
 /**
- *  显示零钱页面
+ *  弹出转账界面
+ *
+ *  @param userInfo 红包页面类型
+ */
+- (void)presentTransferViewControllerWithReceiver:(RedpacketUserInfo *)userInfo;
+
+/**
+ *  弹出转账详情控制器
+ */
+- (void)presentTransferDetailViewController:(RedpacketMessageModel *)model;
+
+/**
+ *  弹出零钱页面
  */
 - (void)presentChangeMoneyViewController;
 
+
+@end
+
+
+/*
+ * 以下方法不再使用，请替换新方法
+ */
+@interface RedpacketViewControl (Deprecated_Nonfunctional)
+
+- (UIViewController *)redpacketViewController __deprecated_msg("请用presentRedPacketViewControllerWithType: memberCount:替换");
+- (UIViewController *)redPacketMoreViewControllerWithGroupMembers:(NSArray *)groupMemberArray __deprecated_msg("请用presentRedPacketViewControllerWithType: memberCount:替换");
+- (void)presentRedPacketMoreViewControllerWithGroupMembers:(NSArray *)groupMemberArray __deprecated_msg("请用presentRedPacketViewControllerWithType: memberCount:替换");
+- (void)presentRedPacketViewController __deprecated_msg("请用presentRedPacketViewControllerWithType: memberCount:替换");
 
 @end
