a32c4ae700a801898e12f54c42b51eb8d94c4a62
XieYajie <xyjxyf@163.com>
2017-01-06 15:29:30 +0800

    添加群组管理员、成员列表页面

---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Class/ChatGroup/Info/EMGroupMembersViewController.m
@@ -0,0 +1,174 @@
+//
+//  EMGroupMembersViewController.m
+//  ChatDemo-UI3.0
+//
+//  Created by XieYajie on 06/01/2017.
+//  Copyright © 2017 XieYajie. All rights reserved.
+//
+
+#import "EMGroupMembersViewController.h"
+
+@interface EMGroupMembersViewController ()<UIActionSheetDelegate, EaseUserCellDelegate>
+
+@property (nonatomic, strong) EMGroup *group;
+@property (nonatomic, strong) NSIndexPath *currentLongPressIndex;
+
+@end
+
+@implementation EMGroupMembersViewController
+
+- (instancetype)initWithGroup:(EMGroup *)aGroup
+{
+    self = [super init];
+    if (self) {
+        self.group = aGroup;
+        [self.dataArray addObjectsFromArray:self.group.admins];
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = NSLocalizedString(@"group.members", @"Members");
+    
+    UIButton *backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
+    backButton.accessibilityIdentifier = @"back";
+    [backButton setImage:[UIImage imageNamed:@"back.png"] forState:UIControlStateNormal];
+    [backButton addTarget:self.navigationController action:@selector(popViewControllerAnimated:) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *backItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
+    [self.navigationItem setLeftBarButtonItem:backItem];
+    
+    self.showRefreshHeader = YES;
+    [self tableViewDidTriggerHeaderRefresh];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.dataArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *CellIdentifier = @"GroupOccupantCell";
+    EaseUserCell *cell = (EaseUserCell *)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[EaseUserCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell.delegate = self;
+    }
+    
+    cell.avatarView.image = [UIImage imageNamed:@"EaseUIResource.bundle/user"];
+    cell.titleLabel.text = [self.dataArray objectAtIndex:indexPath.row];
+    cell.indexPath = indexPath;
+    
+    return cell;
+}
+
+#pragma mark - UIActionSheetDelegate
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == actionSheet.cancelButtonIndex || _currentLongPressIndex == nil) {
+        return;
+    }
+    
+    NSIndexPath *indexPath = _currentLongPressIndex;
+    NSString *userName = [self.dataArray objectAtIndex:indexPath.row];
+    _currentLongPressIndex = nil;
+    
+    [self hideHud];
+    [self showHudInView:self.view hint:NSLocalizedString(@"wait", @"Pleae wait...")];
+    EMError *error = nil;
+    
+    if (buttonIndex == 0) { //移除
+        self.group = [[EMClient sharedClient].groupManager removeOccupants:@[userName] fromGroup:self.group.groupId error:&error];
+    } else if (buttonIndex == 1) { //加入黑名单
+        self.group = [[EMClient sharedClient].groupManager blockOccupants:@[userName] fromGroup:self.group.groupId error:&error];
+    } else if (buttonIndex == 2) {  //禁言
+        EMGroupMuteMember *muteMember = [EMGroupMuteMember createWithUserName:userName muteSeconds:60];
+        self.group = [[EMClient sharedClient].groupManager muteMembers:@[muteMember] fromGroup:self.group.groupId error:&error];
+    } else if (buttonIndex == 3) {  //升为管理员
+        self.group = [[EMClient sharedClient].groupManager addAdmin:userName toGroup:self.group.groupId error:&error];
+    }
+    
+    [self hideHud];
+    if (!error) {
+        [self.dataArray removeObject:userName];
+        [self.tableView reloadData];
+    }
+    else {
+        [self showHint:error.errorDescription];
+    }
+}
+
+#pragma mark - EaseUserCellDelegate
+
+- (void)cellLongPressAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (self.group.membershipType != EMGroupMembershipTypeOwner && self.group.membershipType != EMGroupMembershipTypeAdmin) {
+        return;
+    }
+    
+    self.currentLongPressIndex = indexPath;
+    UIActionSheet *actionSheet = nil;
+    if (self.group.membershipType == EMGroupMembershipTypeOwner) {
+        actionSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:NSLocalizedString(@"cancel", @"Cancel") destructiveButtonTitle:nil  otherButtonTitles:NSLocalizedString(@"group.removeMember", @"Remove from group"), NSLocalizedString(@"friend.block", @"Add to black list"), NSLocalizedString(@"group.toMute", @"Mute 60s"), NSLocalizedString(@"group.addAdmin", @"Add to admin"), nil];
+    } else if (self.group.membershipType == EMGroupMembershipTypeAdmin) {
+        actionSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:NSLocalizedString(@"cancel", @"Cancel") destructiveButtonTitle:nil  otherButtonTitles:NSLocalizedString(@"group.removeMember", @"Remove from group"), NSLocalizedString(@"friend.block", @"Add to black list"), NSLocalizedString(@"group.toMute", @"Mute 60s"), nil];
+    }
+    
+    if (actionSheet) {
+        [actionSheet showInView:[[UIApplication sharedApplication] keyWindow]];
+    }
+}
+
+#pragma mark - data
+
+- (void)tableViewDidTriggerHeaderRefresh
+{
+    self.page = 1;
+    [self fetchMembersWithPage:self.page isHeader:YES];
+}
+
+- (void)tableViewDidTriggerFooterRefresh
+{
+    self.page += 1;
+    [self fetchMembersWithPage:self.page isHeader:NO];
+}
+
+- (void)fetchMembersWithPage:(NSInteger)aPage
+                    isHeader:(BOOL)aIsHeader
+{
+    NSInteger pageSize = 50;
+    __weak typeof(self) weakSelf = self;
+    [self showHudInView:self.view hint:NSLocalizedString(@"loadData", @"Load data...")];
+    [[EMClient sharedClient].groupManager fetchGroupMembersList:self.group.groupId pageNumber:self.page pageSize:pageSize completion:^(NSArray *aMembers, EMError *aError) {
+        [weakSelf hideHud];
+        [weakSelf tableViewDidFinishTriggerHeader:aIsHeader reload:NO];
+        if (!aError) {
+            [weakSelf.dataArray removeAllObjects];
+            [weakSelf.dataArray addObjectsFromArray:aMembers];
+            [weakSelf.tableView reloadData];
+        } else {
+            [weakSelf showHint:NSLocalizedString(@"group.fetchInfoFail", @"failed to get the group details, please try again later")];
+        }
+        
+        if ([aMembers count] < pageSize) {
+            self.showRefreshFooter = NO;
+        } else {
+            self.showRefreshFooter = YES;
+        }
+    }];
+}
+
+@end
