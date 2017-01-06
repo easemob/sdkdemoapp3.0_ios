b830902c5529b5209da79d6539e1d46fa0a11ff9
XieYajie <xyjxyf@163.com>
2017-01-06 16:09:47 +0800

    添加群组禁言和黑名单页面

---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Class/ChatGroup/Info/EMGroupBansViewController.m
@@ -0,0 +1,162 @@
+//
+//  EMGroupBansViewController.m
+//  ChatDemo-UI3.0
+//
+//  Created by XieYajie on 06/01/2017.
+//  Copyright © 2017 XieYajie. All rights reserved.
+//
+
+#import "EMGroupBansViewController.h"
+
+@interface EMGroupBansViewController ()<UIActionSheetDelegate, EaseUserCellDelegate>
+
+@property (nonatomic, strong) EMGroup *group;
+@property (nonatomic, strong) NSIndexPath *currentLongPressIndex;
+
+@end
+
+@implementation EMGroupBansViewController
+
+- (instancetype)initWithGroup:(EMGroup *)aGroup
+{
+    self = [super init];
+    if (self) {
+        self.group = aGroup;
+        [self.dataArray addObjectsFromArray:self.group.admins];
+    }
+    
+    return self;
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = NSLocalizedString(@"title.groupBlackList", @"Black list");
+    
+    UIButton *backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
+    backButton.accessibilityIdentifier = @"back";
+    [backButton setImage:[UIImage imageNamed:@"back.png"] forState:UIControlStateNormal];
+    [backButton addTarget:self.navigationController action:@selector(popViewControllerAnimated:) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *backItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
+    [self.navigationItem setLeftBarButtonItem:backItem];
+    
+    self.showRefreshHeader = YES;
+    [self tableViewDidTriggerHeaderRefresh];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.dataArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *CellIdentifier = @"GroupOccupantCell";
+    EaseUserCell *cell = (EaseUserCell *)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[EaseUserCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell.delegate = self;
+    }
+    
+    cell.avatarView.image = [UIImage imageNamed:@"EaseUIResource.bundle/user"];
+    cell.titleLabel.text = [self.dataArray objectAtIndex:indexPath.row];
+    cell.indexPath = indexPath;
+    
+    return cell;
+}
+
+#pragma mark - UIActionSheetDelegate
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == actionSheet.cancelButtonIndex || _currentLongPressIndex == nil) {
+        return;
+    }
+    
+    NSIndexPath *indexPath = _currentLongPressIndex;
+    NSString *userName = [self.dataArray objectAtIndex:indexPath.row];
+    _currentLongPressIndex = nil;
+    
+    [self hideHud];
+    [self showHudInView:self.view hint:NSLocalizedString(@"wait", @"Pleae wait...")];
+    EMError *error = nil;
+    
+    if (buttonIndex == 0) { //移除
+        self.group = [[EMClient sharedClient].groupManager unblockOccupants:@[userName] forGroup:self.group.groupId error:&error];
+    }
+    
+    [self hideHud];
+    if (!error) {
+        [self.dataArray removeObject:userName];
+        [self.tableView reloadData];
+    }
+    else {
+        [self showHint:error.errorDescription];
+    }
+}
+
+#pragma mark - EaseUserCellDelegate
+
+- (void)cellLongPressAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (self.group.membershipType != EMGroupMembershipTypeOwner && self.group.membershipType != EMGroupMembershipTypeAdmin) {
+        return;
+    }
+    
+    self.currentLongPressIndex = indexPath;
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:NSLocalizedString(@"cancel", @"Cancel") destructiveButtonTitle:nil  otherButtonTitles:NSLocalizedString(@"group.removeBan", @"Remove from blacklist"), nil];;
+    
+    [actionSheet showInView:[[UIApplication sharedApplication] keyWindow]];
+}
+
+#pragma mark - data
+
+- (void)tableViewDidTriggerHeaderRefresh
+{
+    self.page = 1;
+    [self fetchBansWithPage:self.page isHeader:YES];
+}
+
+- (void)tableViewDidTriggerFooterRefresh
+{
+    self.page += 1;
+    [self fetchBansWithPage:self.page isHeader:NO];
+}
+
+- (void)fetchBansWithPage:(NSInteger)aPage
+                 isHeader:(BOOL)aIsHeader
+{
+    NSInteger pageSize = 50;
+    __weak typeof(self) weakSelf = self;
+    [self showHudInView:self.view hint:NSLocalizedString(@"loadData", @"Load data...")];
+    [[EMClient sharedClient].groupManager getGroupBlackListFromServerByID:self.group.groupId pageNumber:self.page pageSize:pageSize completion:^(NSArray *aMembers, EMError *aError) {
+        [weakSelf hideHud];
+        [weakSelf tableViewDidFinishTriggerHeader:aIsHeader reload:NO];
+        if (!aError) {
+            [weakSelf.dataArray removeAllObjects];
+            [weakSelf.dataArray addObjectsFromArray:aMembers];
+            [weakSelf.tableView reloadData];
+        } else {
+            NSString *errorStr = [NSString stringWithFormat:NSLocalizedString(@"group.ban.fetchFail", @"fail to get blacklist: %@"), aError.errorDescription];
+            [weakSelf showHint:errorStr];
+        }
+        
+        if ([aMembers count] < pageSize) {
+            self.showRefreshFooter = NO;
+        } else {
+            self.showRefreshFooter = YES;
+        }
+    }];
+}
+
+@end
