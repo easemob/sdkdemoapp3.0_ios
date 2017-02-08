1b8ffc52c6f5ef346c47b5fa44c89375a288ad04
XieYajie <xyjxyf@163.com>
2017-02-08 18:56:23 +0800

    禁言时间单位换成毫秒

--- ChatDemo-UI3.0/ChatDemo-UI3.0/Class/Chatroom/Info/EMChatroomMembersViewController.m
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Class/Chatroom/Info/EMChatroomMembersViewController.m
@@ -95,7 +95,7 @@
     } else if (buttonIndex == 1) { //加入黑名单
         self.chatroom = [[EMClient sharedClient].roomManager blockMembers:@[userName] fromChatroom:self.chatroom.chatroomId error:&error];
     } else if (buttonIndex == 2) {  //禁言
-        EMMuteMember *muteMember = [EMMuteMember createWithUsername:userName muteSeconds:-1];
+        EMMuteMember *muteMember = [EMMuteMember createWithUsername:userName muteMilliseconds:-1];
         self.chatroom = [[EMClient sharedClient].roomManager muteMembers:@[muteMember] fromChatroom:self.chatroom.chatroomId error:&error];
     } else if (buttonIndex == 3) {  //升为管理员
         self.chatroom = [[EMClient sharedClient].roomManager addAdmin:userName toChatroom:self.chatroom.chatroomId error:&error];
