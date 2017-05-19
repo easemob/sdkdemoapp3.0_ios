9bb3c8816eb47447082c0b85e851cdb3eee77bd1
dujiepeng <347302029@qq.com>
2017-05-19 11:19:32 +0800

    添加发送 分享文件

---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Category/ShareFiles/AppDelegate+ShareFiles.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+ShareFiles.h
+//  ChatDemo-UI3.0
+//
+//  Created by 杜洁鹏 on 15/03/2017.
+//  Copyright © 2017 杜洁鹏. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (ShareFiles)
+
+@end
---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Category/ShareFiles/ShareFilesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareFilesViewController.h
+//  ChatDemo-UI3.0
+//
+//  Created by 杜洁鹏 on 15/03/2017.
+//  Copyright © 2017 杜洁鹏. All rights reserved.
+//
+
+#import "EaseUsersListViewController.h"
+
+@interface ShareFilesViewController : EaseUsersListViewController
+- (instancetype)initWithUrl:(NSURL *)url;
+@end
