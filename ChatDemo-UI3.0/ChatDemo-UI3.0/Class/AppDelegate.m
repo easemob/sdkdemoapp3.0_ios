2e10a7c2fcce69f5f177a7a3ec1dc3b90c248d83
XieYajie <xyjxyf@163.com>
2019-01-16 12:13:03 +0800

    整理SDK初始化代码

--- ChatDemo-UI3.0/ChatDemo-UI3.0/Class/AppDelegate.m
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Class/AppDelegate.m
@@ -10,15 +10,18 @@
  * from Hyphenate Inc.
  */
 
+#import <UserNotifications/UserNotifications.h>
 #import "AppDelegate.h"
 
-#import "AppDelegate+EaseMob.h"
-#import "AppDelegate+Parse.h"
-
 #import <Bugly/Bugly.h>
-#import <UserNotifications/UserNotifications.h>
+
+#import "ChatDemoHelper.h"
 
 #import "EMGlobalVariables.h"
+#import "EMDemoOptions.h"
+
+#import "EMHomeViewController.h"
+#import "EMLoginViewController.h"
 
 @interface AppDelegate () <UNUserNotificationCenterDelegate>
 
@@ -29,31 +32,32 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    if (NSClassFromString(@"UNUserNotificationCenter")) {
-        [UNUserNotificationCenter currentNotificationCenter].delegate = self;
-    }
-
     _connectionState = EMConnectionConnected;
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     
-    // 环信UIdemo中有用到Parse，您的项目中不需要添加，可忽略此处。
-    [self parseApplication:application didFinishLaunchingWithOptions:launchOptions];
-    
-#ifdef DEBUG
-#else
-    //环信Demo中使用Bugly收集crash信息，没有使用cocoapods,库存放在ChatDemo-UI3.0/ChatDemo-UI3.0/3rdparty/Bugly.framework，可自行删除
-    //如果你自己的项目也要使用bugly，请按照bugly官方教程自行配置
-    [Bugly startWithAppId:nil];
-#endif
-    
-    [self easemobApplication:application
-didFinishLaunchingWithOptions:launchOptions
-                 otherConfig:@{kSDKConfigEnableConsoleLogger:[NSNumber numberWithBool:YES]}];
+    [self _initDemo];
+    [self _initHyphenate];
 
     [self.window makeKeyAndVisible];
+    
     return YES;
+}
+
+// 将得到的deviceToken传给SDK
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [[EMClient sharedClient] bindDeviceToken:deviceToken];
+    });
+}
+
+// 注册deviceToken失败，此处失败，与环信SDK无关，一般是您的环境配置或者证书配置有误
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"注册device token失败" message:error.description delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
+    [alert show];
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
@@ -61,7 +65,8 @@ didFinishLaunchingWithOptions:launchOptions
     if (gMainController) {
         [gMainController jumpToChatList];
     }
-    [self easemobApplication:application didReceiveRemoteNotification:userInfo];
+    
+    [[EMClient sharedClient] application:application didReceiveRemoteNotification:userInfo];
 }
 
 - (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
@@ -74,7 +79,7 @@ didFinishLaunchingWithOptions:launchOptions
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
 {
     NSDictionary *userInfo = notification.request.content.userInfo;
-    [self easemobApplication:[UIApplication sharedApplication] didReceiveRemoteNotification:userInfo];
+    [[EMClient sharedClient] application:[UIApplication sharedApplication] didReceiveRemoteNotification:userInfo];
 }
 
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler
@@ -85,5 +90,95 @@ didFinishLaunchingWithOptions:launchOptions
     completionHandler();
 }
 
+#pragma mark - EMPushManagerDelegateDevice
+
+// 打印收到的apns信息
+-(void)didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    NSError *parseError = nil;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:userInfo options:NSJSONWritingPrettyPrinted error:&parseError];
+    NSString *str = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"推送内容" message:str delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
+    [alert show];
+    
+}
+
+#pragma mark - Hyphenate
+
+- (void)_initHyphenate
+{
+    EMDemoOptions *demoOptions = [EMDemoOptions sharedOptions];
+    if (demoOptions.isAutoLogin){
+        gIsInitializedSDK = YES;
+        [[EMClient sharedClient] initializeSDKWithOptions:[demoOptions toOptions]];
+        
+        [[NSNotificationCenter defaultCenter] postNotificationName:KNOTIFICATION_LOGINCHANGE object:@YES];
+    } else {
+        [[NSNotificationCenter defaultCenter] postNotificationName:KNOTIFICATION_LOGINCHANGE object:@NO];
+    }
+}
+
+#pragma mark - Demo
+
+- (void)_initDemo
+{
+#ifdef DEBUG
+#else
+    //环信Demo中使用Bugly收集crash信息，没有使用cocoapods,库存放在ChatDemo-UI3.0/ChatDemo-UI3.0/3rdparty/Bugly.framework，可自行删除
+    //如果你自己的项目也要使用bugly，请按照bugly官方教程自行配置
+    [Bugly startWithAppId:nil];
+#endif
+    
+    //注册本地推送
+    if (NSClassFromString(@"UNUserNotificationCenter")) {
+        [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+    }
+    
+    //注册登录状态监听
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(loginStateChange:) name:KNOTIFICATION_LOGINCHANGE object:nil];
+    
+    [[EaseSDKHelper shareHelper] hyphenateApplication:nil didFinishLaunchingWithOptions:nil];
+}
+
+- (void)loginStateChange:(NSNotification *)aNotif
+{
+    UINavigationController *navigationController = nil;
+    
+    BOOL loginSuccess = [aNotif.object boolValue];
+    if (loginSuccess) {//登录成功加载主窗口控制器
+        ChatDemoHelper *demoHelper = [ChatDemoHelper shareHelper];
+        //加载申请通知的数据
+        [[ApplyViewController shareController] loadDataSourceFromLocalDB];
+        
+        if (gMainController == nil) {
+            //            MainViewController *mainController = [[MainViewController alloc] init];
+            //            [EMGlobalVariables setGlobalMainController:mainController];
+            //            navigationController = [[EMNavigationController alloc] initWithRootViewController:mainController];
+            
+            EMHomeViewController *homeController = [[EMHomeViewController alloc] init];
+            navigationController = [[UINavigationController alloc] initWithRootViewController:homeController];
+        } else {
+            navigationController = gMainController.navigationController;
+        }
+        
+        [demoHelper asyncGroupFromServer];
+        [demoHelper asyncConversationFromDB];
+        [demoHelper asyncPushOptions];
+    } else {//登录失败加载登录页面控制器
+        if (gMainController) {
+            [gMainController.navigationController popToRootViewControllerAnimated:NO];
+        }
+        [EMGlobalVariables setGlobalMainController:nil];
+        
+        EMLoginViewController *controller = [[EMLoginViewController alloc] init];
+        navigationController = [[UINavigationController alloc] initWithRootViewController:controller];
+        
+        [[UINavigationBar appearance] setTitleTextAttributes:
+         [NSDictionary dictionaryWithObjectsAndKeys:[UIColor blackColor], NSForegroundColorAttributeName, [UIFont systemFontOfSize:18], NSFontAttributeName, nil]];
+    }
+    
+    self.window.rootViewController = navigationController;
+}
 
 @end
