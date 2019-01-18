31d234f8b2eeae7bcb260e45ceb3217215fdf4be
XieYajie <xyjxyf@163.com>
2019-01-18 15:27:18 +0800

    添加自定义消息模型

---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Class/Home/Chat/EMMessageModel.h
@@ -0,0 +1,17 @@
+//
+//  EMMessageModel.h
+//  ChatDemo-UI3.0
+//
+//  Created by XieYajie on 2019/1/18.
+//  Copyright © 2019 XieYajie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EMMessageModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Class/Home/Chat/EMMessageModel.m
@@ -0,0 +1,13 @@
+//
+//  EMMessageModel.m
+//  ChatDemo-UI3.0
+//
+//  Created by XieYajie on 2019/1/18.
+//  Copyright © 2019 XieYajie. All rights reserved.
+//
+
+#import "EMMessageModel.h"
+
+@implementation EMMessageModel
+
+@end
